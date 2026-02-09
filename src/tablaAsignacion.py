5ed8a6454c981f1ab2702508e42d52fe7f29d8ac
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-09 14:22:18 +0100

    feat(tabla_asignacion): :sparkles: add functions

--- src/tablaAsignacion.py
+++ src/tablaAsignacion.py
@@ -27,3 +27,24 @@ class TablaAsignacion:
             "E",
         )
     
+    def getTabla(self):
+        return self.tabla
+    
+    def getLetra(self, posicion):
+        try:
+            return self.tabla[posicion]
+        except IndexError:
+            return "Posicion letra fuera de rango"
+
+    def calcularLetra(self, cifras):
+        return TablaAsignacion.getTabla(self)[int(cifras) % len(TablaAsignacion.getTabla(self))]
+    
+    def getModulo(self):
+        return (len(TablaAsignacion.getTabla(self)))
+    
+    def isLetraPermitida(self, letra):
+        return letra in TablaAsignacion.getTabla(self)
+    
+
+    def __repr__(self):
+        return "Tabla de asignación"
