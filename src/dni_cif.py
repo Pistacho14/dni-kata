b3e46934d61a0c8c3e1a521fce856c031e3d28e1
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-09 14:18:54 +0100

    feat(dni_cif): :sparkles: add constuctor

---
+++ src/dni_cif.py
@@ -0,0 +1,11 @@
+from src.tablaAsignacion import TablaAsignacion
+
+class Dni:
+    
+    LONGITUD_NUMEROS_DNI = 8
+    
+    def __init__(self):
+        self.dni = ''
+        self.numeroSano = 0
+        self.letraSana = ''
+
