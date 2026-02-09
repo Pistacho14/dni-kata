8c577fcaff02021b863d3240ea010e0dd06b4b42
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-09 14:20:44 +0100

    feat(dni_cif): :sparkles: add new functions

--- src/dni_cif.py
+++ src/dni_cif.py
@@ -26,3 +26,20 @@ class Dni:
     
     def setLetraSana(self, letraSana):
         self.letraSana = letraSana
+    
+    def _sanearNumero(self):
+        Dni.setNumeroSano(self, Dni.getDni(self)[:-1])
+        
+    def _sanearLetra(self):
+        Dni.setLetraSana(self, Dni.getDni(self)[-1])
+        
+    def _checkLetra(self):
+        Dni._sanearLetra(self)
+        Dni._sanearNumero(self)
+        return TablaAsignacion.calcularLetra(TablaAsignacion(), Dni.getNumeroSano(self))
+    
+    def _checkLongitud(self):
+        return len(Dni.getNumeroSano(self)) == Dni.LONGITUD_NUMEROS_DNI
+    
+    def checkCIF(self):
+        return Dni._checkLetra(self) and Dni._checkLongitud(self)
