81d7fdfaa2f43b69d7386832c794df67356ae62c
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-09 14:20:21 +0100

    feat(dni_cif): :sparkles: add getters and setters

--- src/dni_cif.py
+++ src/dni_cif.py
@@ -9,3 +9,20 @@ class Dni:
         self.numeroSano = 0
         self.letraSana = ''
 
+    def getDni(self):
+        return self.dni
+    
+    def setDni(self, dni):
+        self.dni = dni
+
+    def getNumeroSano(self):
+        return self.numeroSano
+    
+    def setNumeroSano(self, numeroSano):
+        self.numeroSano = numeroSano
+    
+    def getLetraSana(self):
+        return self.letraSana
+    
+    def setLetraSana(self, letraSana):
+        self.letraSana = letraSana
