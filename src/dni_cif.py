f52a6502ed0e265f22408ff51b413c41762e7fbf
Pablo González <pablogonzaleztemes@gmail.com>
2026-02-09 19:43:37 +0100

    feat(dni_cif): :sparkles: add new function

--- src/dni_cif.py
+++ src/dni_cif.py
@@ -6,8 +6,10 @@ class Dni:
 
     def __init__(self):
         self.dni = ""
-        self.numeroSano = 0
-        self.letraSana = ""
+        self.numeroDni = ""
+        self.letraDni = ""
+        self.numeroSano = False
+        self.letraSana = False
 
     def getDni(self):
         return self.dni
@@ -15,31 +17,47 @@ class Dni:
     def setDni(self, dni):
         self.dni = dni
 
+    def getNumeroDni(self):
+        return self.numeroDni
+
+    def setNumeroDni(self, numeroDni):
+        self.numeroDni = numeroDni
+
+    def getLetraDni(self):
+        return self.letraDni
+
+    def setLetraDni(self, letraDni):
+        self.letraDni = letraDni
+
     def getNumeroSano(self):
         return self.numeroSano
-
+    
     def setNumeroSano(self, numeroSano):
         self.numeroSano = numeroSano
-
+    
     def getLetraSana(self):
         return self.letraSana
-
+    
     def setLetraSana(self, letraSana):
         self.letraSana = letraSana
-
-    def _sanearNumero(self):
-        Dni.setNumeroSano(self, Dni.getDni(self)[:-1])
-
-    def _sanearLetra(self):
-        Dni.setLetraSana(self, Dni.getDni(self)[-1])
+    
+    def _separarDni(self):
+        Dni.setNumeroDni(self, Dni.getDni(self)[:-1])
+        Dni.setLetraDni(self, Dni.getDni(self)[-1])
 
     def _checkLetra(self):
-        Dni._sanearLetra(self)
-        Dni._sanearNumero(self)
-        return TablaAsignacion.calcularLetra(TablaAsignacion(), Dni.getNumeroSano(self))
+        Dni._separarDni(self)
+        return TablaAsignacion.calcularLetra(TablaAsignacion(), Dni.getNumeroDni(self)) == Dni.getLetraDni(self)
 
     def _checkLongitud(self):
-        return len(Dni.getNumeroSano(self)) == Dni.LONGITUD_NUMEROS_DNI
+        return len(Dni.getNumeroDni(self)) == Dni.LONGITUD_NUMEROS_DNI
 
     def checkCIF(self):
-        return Dni._checkLetra(self) and Dni._checkLongitud(self)
+        return Dni._sanearNumero(self) and Dni._checkLetra(self) and Dni._checkLongitud(self)
+    
+    def checkDni(self):
+        return Dni._sanearNumero(self) and Dni._checkLetra(self) and Dni._checkLongitud(self)
+    
+    def _sanearNumero(self):
+        Dni._separarDni(self)
+        return Dni.getNumeroDni(self).isnumeric()
