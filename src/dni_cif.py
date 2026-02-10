486701982517b48aaa01d9a2e4e70e1ddc3bdc60
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-02-10 10:21:29 +0100

    style(dni_cif): :art: format code

--- src/dni_cif.py
+++ src/dni_cif.py
@@ -31,39 +31,45 @@ class Dni:
 
     def getNumeroSano(self):
         return self.numeroSano
-    
+
     def setNumeroSano(self, numeroSano):
         self.numeroSano = numeroSano
-    
+
     def getLetraSana(self):
         return self.letraSana
-    
+
     def setLetraSana(self, letraSana):
         self.letraSana = letraSana
-    
+
     def _separarDni(self):
         Dni.setParteNumericaDni(self, Dni.getDni(self)[:-1])
         Dni.setparteAlfabeticaDni(self, Dni.getDni(self)[-1])
 
     def checkLetra(self):
         Dni._separarDni(self)
-        return Dni._sanearNumero(self) and TablaAsignacion.calcularLetra(TablaAsignacion(), Dni.getParteNumericaDni(self)) == Dni.getParteAlfabeticaDni(self)
+        return Dni._sanearNumero(self) and TablaAsignacion.calcularLetra(
+            TablaAsignacion(), Dni.getParteNumericaDni(self)
+        ) == Dni.getParteAlfabeticaDni(self)
 
     def checkLongitud(self):
         return len(Dni.getParteNumericaDni(self)) == Dni.LONGITUD_NUMEROS_DNI
 
     def checkCIF(self):
-        return Dni._sanearNumero(self) and Dni.checkLetra(self) and Dni.checkLongitud(self)
-    
+        return (
+            Dni._sanearNumero(self) and Dni.checkLetra(self) and Dni.checkLongitud(self)
+        )
+
     def checkDni(self):
-        return Dni._sanearNumero(self) and Dni.checkLetra(self) and Dni.checkLongitud(self)
-    
+        return (
+            Dni._sanearNumero(self) and Dni.checkLetra(self) and Dni.checkLongitud(self)
+        )
+
     def _sanearNumero(self):
         Dni._separarDni(self)
         return Dni.getParteNumericaDni(self).isnumeric()
 
     def obtenerLetra(self):
         return str(Dni.getParteAlfabeticaDni(self)) if Dni.checkDni(self) else None
-    
+
     def __repr__(self):
         return Dni.getDni(self)
